f5a943fd97884f4cec239e1f989de4e0aa7c6fe3
Samarth622 <guptasamarth709@gmail.com>
2026-09-18 14:54:23 +0530

    create all controller methods in content-service:

--- content-service/src/main/java/com/netflix/contentService/controller/ContentController.java
+++ content-service/src/main/java/com/netflix/contentService/controller/ContentController.java
@@ -1,4 +1,59 @@
 package com.netflix.contentService.controller;
 
+import com.netflix.contentService.dto.MovieRequest;
+import com.netflix.contentService.dto.MovieResponse;
+import com.netflix.contentService.model.Genre;
+import com.netflix.contentService.service.ContentService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+@Slf4j
+@RequiredArgsConstructor
 public class ContentController {
+
+    private final ContentService contentService;
+
+    // Add new movie to catalog
+    @PostMapping
+    public ResponseEntity<MovieResponse> addMovie(
+            @Valid @RequestBody MovieRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(contentService.addMovie(request));
+    }
+
+    // Get All Movies
+    @GetMapping
+    public ResponseEntity<List<MovieResponse>> getAllMovies() {
+        return ResponseEntity.ok(contentService.getAllMovies());
+    }
+
+    // Get movie by genre
+    @GetMapping("/genre/{genre}")
+    public ResponseEntity<List<MovieResponse>> getMoviesByGenre(
+            @PathVariable Genre genre) {
+        return ResponseEntity.ok(contentService.getMoviesByGenre(genre));
+    }
+
+    // Get movie by ID
+    @GetMapping("/{movieId}")
+    public ResponseEntity<MovieResponse> getMoviesById(
+            @PathVariable String movieId) {
+        return ResponseEntity.ok(contentService.getMoviesById(movieId));
+    }
+
+    //Search Movies
+    @GetMapping("/search")
+    public ResponseEntity<List<MovieResponse>> searchMovies(
+            @RequestParam String title) {
+
+        return ResponseEntity.ok(contentService.searchMovies(title));
+    }
 }
---
+++ content-service/src/main/java/com/netflix/contentService/service/ContentService.java
@@ -0,0 +1,7 @@
+package com.netflix.contentService.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContentService {
+}
